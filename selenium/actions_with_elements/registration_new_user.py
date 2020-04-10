8a97d1104e4fe2d25ca662fa06718459b09aed67
aminzin <jiganoaldog@gmail.com>
2020-04-10 14:24:58 +0300

    edit by_css_selector -> by_name

--- selenium/actions_with_elements/registration_new_user.py
+++ selenium/actions_with_elements/registration_new_user.py
@@ -20,22 +20,22 @@ try:
 
     sleep(1)
 
-    first_name = browser.find_element_by_css_selector("[name='firstname']")
+    first_name = browser.find_element_by_name("firstname")
     first_name.send_keys('first')
 
-    last_name = browser.find_element_by_css_selector("[name='lastname']")
+    last_name = browser.find_element_by_name("lastname")
     last_name.send_keys('last')
 
-    address_1 = browser.find_element_by_css_selector("[name='address1']")
+    address_1 = browser.find_element_by_name("address1")
     address_1.send_keys('address1')
 
-    postcode = browser.find_element_by_css_selector("[name='postcode']")
+    postcode = browser.find_element_by_name("postcode")
     postcode.send_keys('11111')
 
-    city = browser.find_element_by_css_selector("[name='city']")
+    city = browser.find_element_by_name("city")
     city.send_keys('Washington')
 
-    phone = browser.find_element_by_css_selector("[name='phone']")
+    phone = browser.find_element_by_name("phone")
     phone.send_keys('1-111-111-1111')
 
     country = browser.find_element_by_css_selector(".select2-selection__rendered")
@@ -45,25 +45,25 @@ try:
     country_input.send_keys('United States')
     country_input.send_keys(Keys.ENTER)
 
-    email_registration_page = browser.find_element_by_css_selector("[name='email']")
+    email_registration_page = browser.find_element_by_name("email")
     email_registration_page.send_keys(email_address)
 
-    password_registration_page = browser.find_element_by_css_selector("[name='password']")
+    password_registration_page = browser.find_element_by_name("password")
     password_registration_page.send_keys(password_text)
 
-    confirmed_password = browser.find_element_by_css_selector("[name='confirmed_password']")
+    confirmed_password = browser.find_element_by_name("confirmed_password")
     confirmed_password.send_keys(password_text)
 
-    create_button = browser.find_element_by_css_selector("[name='create_account']")
+    create_button = browser.find_element_by_name("create_account")
     create_button.click()
 
-    password_registration_page = browser.find_element_by_css_selector("[name='password']")
+    password_registration_page = browser.find_element_by_name("password")
     password_registration_page.send_keys(password_text)
 
-    confirmed_password = browser.find_element_by_css_selector("[name='confirmed_password']")
+    confirmed_password = browser.find_element_by_name("confirmed_password")
     confirmed_password.send_keys(password_text)
 
-    create_button = browser.find_element_by_css_selector("[name='create_account']")
+    create_button = browser.find_element_by_name("create_account")
     create_button.click()
 
     sleep(1)
@@ -73,13 +73,13 @@ try:
 
     sleep(1)
 
-    email_main_page = browser.find_element_by_css_selector("[name='email']")
+    email_main_page = browser.find_element_by_name("email")
     email_main_page.send_keys(email_address)
 
-    password_main_page = browser.find_element_by_css_selector("[name='password']")
+    password_main_page = browser.find_element_by_name("password")
     password_main_page.send_keys(password_text)
 
-    login = browser.find_element_by_css_selector("[name='login']")
+    login = browser.find_element_by_name("login")
     login.click()
 
     sleep(1)
